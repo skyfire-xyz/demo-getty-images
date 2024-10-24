3e4388d0909f497835395d80c4cd256f8415d47a
Koji Miyauchi <koji@supermojo.com>
2024-10-24 14:08:45 -0400

    improvements

--- lib/getty-images/ai-agent/tools/purchase-images/purchase-images.tsx
+++ lib/getty-images/ai-agent/tools/purchase-images/purchase-images.tsx
@@ -13,13 +13,14 @@ interface PurchaseImagesConfig {
 export class PurchaseImagesTool extends BaseTool {
   public static readonly toolName = "purchase_images"
   public static readonly instruction = `
-    When the user asks you to purchase image or images:
-    1. First, respond to the user acknowledging their request and informing them that you'll initiate the purchase for those images. If user didn't specify the sizes of the image, you show the list of the available sizes for the user to choose. (also pricing of those images)
-    2. If user specify the size of the image, respond to the user that acknowledge their request and inform them that you'll initiate the purchase for those images and sizes.
-    3. Then, execute the "purchase_images" tool with imageIDs as the parameter. Image ID can be found in the JSON object that's been sent before asking the question.
-
-    Make sure to purchase images that you and the user have just discussed. Also, if you are purchasing more than 5 images, you need to confirm with the user before proceeding with the purchase.
-    Make sure to confirm the sizes, if not specified by the user.
+When the user asks you to purchase image or images:
+1. First, respond to the user acknowledging their request and informing them that you'll initiate the purchase for those images. If the user didn't specify the sizes of the image, show the list of the available sizes for the user to choose. (also pricing of those images)
+2. If the user specifies the size of the image, respond to the user acknowledging their request and inform them that you'll initiate the purchase for those images and sizes.
+3. Then, execute the "purchase_images" tool with imageIDs as the parameter. Image ID can be found in the JSON object that's been sent before asking the question.
+Make sure to purchase only the images that you and the user have just discussed in this most recent conversation. Do not purchase images from previous conversations or unrelated discussions. Specifically, if a command like "buy them all" is given, ensure it refers to images discussed in this latest interaction only.
+5. If you are purchasing more than 5 images, you need to confirm with the user before proceeding with the purchase.
+6. Make sure to confirm the sizes, if not specified by the user.
+7. Before executing the purchase, briefly summarize the images being purchased to ensure they match the recent discussion.
   `
 
   public override createTool() {
